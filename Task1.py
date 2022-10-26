88259d1645e7f8fa34bd3170195eda70d4dfb10a
Nis_U <nisnab.udas@gmail.com>
2022-10-26 10:25:01 +0200

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.markdown("# Task 1 ❄️")
+st.sidebar.markdown("# Task 1 ❄️")
+# Using object notation
+questions=["RQ1","RQ2","RQ3","RQ4","RQ5"]
+    
+choice=st.sidebar.selectbox("Select questions",questions)
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+st.title('DeinHaus Data Explorer')
+st.text('This is a web app to allow exploration of deinhaus data')
+
+#cretae file uploader
+
+upload_file=st.file_uploader('Upload a csv here')
+if upload_file is not None:
+    df=pd.read_csv(upload_file)
+    
+    if choice=='RQ1':
+        st.title('RQ1')
+        st.text('Can statements be made about data quality?')
+        # Create a section for the dataframe statistics
+        st.header('Statistics of Dataframe')
+        st.write(df.describe())
+        # Create a section for the dataframe header
+        st.header('Header of Dataframe')
+        st.write(df.head())
+       
+    elif choice=='RQ2':
+        st.header('research 2')        
